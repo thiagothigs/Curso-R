cac3805bda28f55bdac19b9dadc2722e2fe277d8
thiagoferraz <thiagothigs@gmail.com>
2018-07-04 02:16:43 -0300

    Initial commit

---
+++ aula01_operacoesMat.R
@@ -0,0 +1,17 @@
+# aula 01 - Operações matemáticas
+# operações essenciais da matemática
+
+# adição
+4+4
+# subtração
+4-4
+# multiplicação
+4*4
+# divisão
+4/4
+# resto
+4%%4
+#exponenciação
+4^4 # ou 4**4
+# ordem parênteses
+4*(4-4)
