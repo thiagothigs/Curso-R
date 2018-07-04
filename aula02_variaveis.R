29f177436cb5b69dae3bc1b89dfd6039a6044be1
thiagoferraz <thiagothigs@gmail.com>
2018-07-04 02:38:20 -0300

    aula02
    atribuição de variáveis

---
+++ aula02_variaveis.R
@@ -0,0 +1,14 @@
+# aula 02 - criando variáveis
+
+# atribuição de valores / criação de variáveis
+
+variavel <- 10 # operador <- (atribui um valor)
+variavel # imprime no workspace o valor 10
+
+# atribuir outro tipo de valor na variável
+variavel <- "churros" # character
+variavel
+
+# resultado de um cálculo
+x <- 20^2
+x/4
